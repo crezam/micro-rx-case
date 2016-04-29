40cfc4cd86f6ecb88b9b2137ad2195e9915c9e47
Camilo Crespo <crezam@live.com>
2016-04-29 16:45:43 -0400

    Vert.x http server

--- src/main/java/com/crezam/ordering/api/EmbeddedApi.java
+++ src/main/java/com/crezam/ordering/api/EmbeddedApi.java
@@ -1,8 +1,26 @@
 package com.crezam.ordering.api;
 
+import io.vertx.core.Vertx;
+import java.util.Random;
+
 public class EmbeddedApi {
 
-    public static void main(String[] args){
-        System.out.println("test");
+    public static void main(String[] args) {
+        Vertx.vertx()
+                .createHttpServer()
+                .requestHandler(req -> req.response().end(Integer.toString(mockPrice())))
+                .listen(8080, handler -> {
+                    if (handler.succeeded()) {
+                        System.out.println("OK");
+                    } else {
+                        System.err.println("Alert");
+                    }
+                });
     }
+
+    private static int mockPrice() {
+        Random r = new Random();
+        return r.nextInt(1000);
+    }
+
 }
